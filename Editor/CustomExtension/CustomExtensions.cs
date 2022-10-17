68aa038b441b126de822a5c08af6f78e57676188
kiltec <53696055+pwnedu@users.noreply.github.com>
2022-10-17 15:54:13 +1100

    Update CustomExtensions.cs
    added extra icons

--- Editor/CustomExtension/CustomExtensions.cs
+++ Editor/CustomExtension/CustomExtensions.cs
@@ -9,6 +9,23 @@ namespace CustomExtensions
         private static readonly string diamond = "◆";
         private static readonly string heart = "♥︎";
         private static readonly string triangle = "◀";
+        private static readonly string sparkle = "✦";
+
+        private static readonly string star = "★";
+        private static readonly string sun = "☀";
+        private static readonly string moon = "☽";
+        private static readonly string cloud = "☁";
+        private static readonly string snow = "❆";
+
+        private static readonly string clover = "✤";
+        private static readonly string flower = "✿";
+        private static readonly string point = "☚";
+        private static readonly string pen = "✎";
+
+        private static readonly string hazard = "☢";
+        private static readonly string peace = "✇";
+        private static readonly string skull = "☠";
+        private static readonly string target = "☉";
 
         public static Rect CalculateRect(Rect selection, float leftOffset, float rightOffset, float backgroundHeight)
         {
@@ -41,6 +58,8 @@ namespace CustomExtensions
 
             switch (icon)
             {
+                case Icon.None:
+                    break;
                 case Icon.Bullet:
                     GUI.Label(iconRect, bullet, style);
                     break;
@@ -52,6 +71,48 @@ namespace CustomExtensions
                     break;
                 case Icon.Triangle:
                     GUI.Label(iconRect, triangle, style);
+                    break;
+                case Icon.Sparkle:
+                    GUI.Label(iconRect, sparkle, style);
+                    break;
+                case Icon.Star:
+                    GUI.Label(iconRect, star, style);
+                    break;
+                case Icon.Sun:
+                    GUI.Label(iconRect, sun, style);
+                    break;
+                case Icon.Moon:
+                    GUI.Label(iconRect, moon, style);
+                    break;
+                case Icon.Cloud:
+                    GUI.Label(iconRect, cloud, style);
+                    break;
+                case Icon.Snow:
+                    GUI.Label(iconRect, snow, style);
+                    break;
+                case Icon.Clover:
+                    GUI.Label(iconRect, clover, style);
+                    break;
+                case Icon.Flower:
+                    GUI.Label(iconRect, flower, style);
+                    break;
+                case Icon.Point:
+                    GUI.Label(iconRect, point, style);
+                    break;
+                case Icon.Pen:
+                    GUI.Label(iconRect, pen, style);
+                    break;
+                case Icon.Hazard:
+                    GUI.Label(iconRect, hazard, style);
+                    break;
+                case Icon.Peace:
+                    GUI.Label(iconRect, peace, style);
+                    break;
+                case Icon.Skull:
+                    GUI.Label(iconRect, skull, style);
+                    break;
+                case Icon.Target:
+                    GUI.Label(iconRect, target, style);
                     break;
                 default:
                     break;
@@ -81,6 +142,33 @@ namespace CustomExtensions
         public float xAdjust;
     }
 
-    public enum InfoAnchor { Top, Middle, Bottom }
-    public enum Icon { None, Bullet, Diamond, Heart, Triangle }
+    public enum InfoAnchor 
+    { 
+        Top, 
+        Middle, 
+        Bottom 
+    }
+
+    public enum Icon 
+    { 
+        None, 
+        Bullet, 
+        Diamond, 
+        Heart, 
+        Triangle, 
+        Sparkle, 
+        Star, 
+        Sun, 
+        Moon, 
+        Cloud, 
+        Snow, 
+        Clover, 
+        Flower, 
+        Point, 
+        Pen, 
+        Hazard, 
+        Peace,
+        Skull,
+        Target
+    }
 }
